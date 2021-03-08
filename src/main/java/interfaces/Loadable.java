acc2f4985cef03980844639a354a213771a47802
theTechOwl <david.e.chavarro@outlook.com>
2021-03-08 15:41:42 -0500

    Fixed a bug where the directory in infoRetrievable.java was pointing to an invalid directory.

--- src/main/java/interfaces/Loadable.java
+++ src/main/java/interfaces/Loadable.java
@@ -2,8 +2,8 @@ package interfaces;
 //Holds attributes needed to allow file scanner to load data files.
 public interface Loadable {
 	public final String FILE_COMMENT = "##";
-	public final String LINUX_DATA_PATH = "/media/the_tech_owl/DATA/Documents/GitHub/BitOwlBot/data/";
-	public final String WINDOWS_DATA_PATH = "D:\\Documents\\GitHub\\BitOwlBot\\data\\";
+	public final String LINUX_DATA_PATH = "/media/the_tech_owl/DATA/Documents/GitHub/theTechOwl/BitOwlBot/data/";
+	public final String WINDOWS_DATA_PATH = "D:\\Documents\\GitHub\\thetTechOwl\\BitOwlBot\\data\\";
 	
 	public String GET_DATA_PATH();
 }
