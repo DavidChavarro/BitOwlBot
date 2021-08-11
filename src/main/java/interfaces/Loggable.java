02c9b451b280f838607f3a3bd3af5fe4f752a06f
theTechOwl <david.e.chavarro@outlook.com>
2021-08-11 16:29:28 -0400

    Made this branch a new slate to convert program into python.

--- src/main/java/interfaces/Loggable.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Loggable {
-	//Stores information related to logging program errors.
-public final String LINUX_LOG_PATH = "devLogs/";
-public final String WINDOWS_LOG_PATH = "devlogs\\";
-}
--- src/main/java/testerPkg/Tester.java
+++
@@ -1,6 +0,0 @@
-package testerPkg;
-//Abstract class used to test program's classes.
-//Contains universal methods for derived subclasses.
-public abstract class Tester {
-	
-}
