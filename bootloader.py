813586fdd0810116dc3dd1085a479d31dd9ebd7f
David Chavarro <david.e.chavarro@outlook.com>
2022-12-26 22:33:55 -0500

    Update bootloader.py

--- bootloader.py
+++ bootloader.py
@@ -1,5 +1,29 @@
 from discord_handler.discord_main import quotes
+from multiprocessing import Process
 import os
 
-quotes.get_bot.run(os.getenv('BITOWL_TOKEN'))
+BOT_TOKEN: str = ""
 
+# Show the control panel GUI for bot.
+CTRL_PN_GUI: str = FALSE
+
+# Display splash screen if applicable
+if CTRL_PN_GUI is TRUE:
+    #splash_scr.show()
+
+
+#Get Discord bot token and run Discord Bot.
+try:
+    BOT_TOKEN = os.getenv('BITOWL_TOKEN')
+except FileError as fe:
+    # BOT_TOKEN = get_tok_inp()
+finally:
+    if CTRL_PN_GUI is true:
+        #splash_scr.close()
+        gui_proc = #Process(target=gui.start, args=(,))
+    core_proc = #Process(target=quotes.get_bot.run, args=(BOT_TOKEN,)
+    core_proc.start()
+    if CTRL_PN_GUI:
+        gui_proc.start()
+        gui_proc.join()
+    core_proc.start()
